e233b279f7130f3c33124f660039e2c995dc7210
Mercedes Egido <meegido@gmail.com>
2017-07-26 19:53:04 +0200

    rover turns left with North default

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,7 +5,15 @@ var rover = {
 
 
 function turnLeft(rover){
-
+  if(rover.direction === "N") {
+    rover.direction = "W";
+  } else if (rover.direction === "W") {
+    rover.direction = "S";
+  } else if (rover.direction === "S") {
+    rover.direction = "E";
+  } else if (rover.direction === "E") {
+    rover.direction = "N";
+  }
 }
 
 function turnRight(rover){
