ab0758cd58fd9d252485121ec2b0f034cd950331
Mercedes Egido <meegido@gmail.com>
2017-07-26 19:51:59 +0200

    rover object

---
+++ javascripts/app.js
@@ -0,0 +1,21 @@
+var rover = {
+  direction: "N",
+
+}
+
+
+function turnLeft(rover){
+
+}
+
+function turnRight(rover){
+
+}
+
+function moveForward(rover){
+
+}
+
+
+console.log(rover);
+turnRight(rover);
